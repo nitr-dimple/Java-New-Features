856fd9f3ea45d6ada5d91b014b7e245b2c1b61bc
Dimpleben Kanjibhai Patel <patel.dim@northeastern.edu>
2024-02-01 00:46:10 -0500

    Suppressed Exceptions

---
+++ Java Features/src/main/java/org/com/SuppressedExceptions/SuppressedExceptions.java
@@ -0,0 +1,60 @@
+package org.com.SuppressedExceptions;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+/**
+ * @author Dimpleben Kanjibhai Patel
+ */
+public class SuppressedExceptions {
+
+    private final static Logger LOGGER = Logger.getLogger(SuppressedExceptions.class.getName());
+
+    public static void main(String[] args) {
+        try{
+            beforeJava7();
+        }catch (Exception ex){
+            LOGGER.log(Level.SEVERE, ex.getMessage());
+        }
+
+        LOGGER.log(Level.INFO, "============================================================================");
+
+        try{
+            withJava7();
+        }catch (Exception ex){
+            LOGGER.log(Level.SEVERE, ex.getMessage());
+            final Throwable[] suppressedExceptions = ex.getSuppressed();
+            final int numSuppressed = suppressedExceptions.length;
+            if(numSuppressed > 0){
+                for(final Throwable exception: suppressedExceptions){
+                    LOGGER.log(Level.SEVERE, exception.getMessage());
+                }
+            }
+        }
+    }
+
+    /***
+     * Implementation befor Java 7
+     * @throws Exception
+     */
+    public static void beforeJava7() throws Exception{
+        CustomDirtyResource cr = null;
+        try{
+            cr = new CustomDirtyResource();
+            cr.readFromResource();
+        }finally {
+            cr.close();
+        }
+    }
+
+    /***
+     * Implementation from Java 7
+     * @throws Exception
+     */
+    public static void withJava7() throws Exception{
+        try( CustomDirtyResource cr = new CustomDirtyResource();){
+            cr.readFromResource();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 ## Java7
 
 1. [Try With Resource](https://github.com/nitr-dimple/Java-New-Features/tree/master/Java%20Features/src/main/java/org/com/java7/TryWithResources)
+2. [Suppressed Exceptions](https://github.com/nitr-dimple/Java-New-Features/tree/master/Java%20Features/src/main/java/org/com/java7/suppressedExceptions.java)
 
