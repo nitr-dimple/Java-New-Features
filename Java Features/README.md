40e9576e1526543169d9fecb6c20f12bf74f0ebf
Dimpleben Kanjibhai Patel <patel.dim@northeastern.edu>
2024-01-31 22:11:02 -0500

    Java7 - Try with Resource

--- Java Features/README.md
+++ Java Features/README.md
@@ -2,3 +2,5 @@
 
 ## Java7
 
+1. [Try With Resource](https://github.com/nitr-dimple/Java-New-Features/tree/master/Java%20Features/src/main/java/org/com/java7/TryWithResources)
+
---
+++ Java Features/src/main/java/org/com/SuppressedExceptions/CustomDirtyResource.java
@@ -0,0 +1,17 @@
+package org.com.SuppressedExceptions;
+
+/**
+ * @author Dimpleben Kanjibhai Patel
+ */
+public class CustomDirtyResource implements  AutoCloseable{
+
+    @Override
+    public void close() throws Exception {
+        throw new NullPointerException("OOPs. It is very bad to have a NullPointerException !!");
+    }
+
+    public void readFromResource(){
+        throw new RuntimeException("I am sorry. I don't have data available right now due to network issue!!!");
+    }
+
+}
